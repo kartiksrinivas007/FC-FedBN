e8404ff43fe72c7e83a96b683cab00421bf6293c
kartiksrinivas007 <kartiksrinivas008@gmail.com>
2023-09-13 12:52:12 +0530

    Change Shell config

--- fccl+/run.sh
+++ fccl+/run.sh
@@ -17,7 +17,8 @@
 #python main.py --model fcclwe  --device_id 6 --dataset fl_officehome --structure heterogeneity --csv_log&
 #python main.py --model fcclkeyl  --device_id 7 --dataset fl_digits --structure homogeneity --csv_log
 #python main.py --model fcclwe  --device_id 7 --dataset fl_digits --structure homogeneity --csv_log&
-python main.py --model fccl  --device_id 0 --dataset fl_digits --structure heterogeneity --csv_log
+# python main.py --model fccl  --device_id 0 --dataset fl_digits --structure heterogeneity --csv_log
+python main.py --model feddf  --device_id 0 --dataset fl_digits --structure heterogeneity --csv_log
 # python main.py --model fccl  --device_id 1 --dataset fl_office31 --structure homogeneity --csv_log&
 
 
--- fccl+/run_new.sh
+++ fccl+/run_new.sh
@@ -1,5 +1,5 @@
 
-#python main.py --model fedmd  --device_id 0 --dataset fl_digits --structure homogeneity --csv_log &
+python main.py --model fedmd  --device_id 0 --dataset fl_digits --structure heterogeneity --csv_log
 #python main.py --model fedmd  --device_id 1 --dataset fl_office31 --public_dataset pub_cifar100 --structure heterogeneity --csv_log &
 
 #python main.py --model fcclsim  --device_id 0 --dataset fl_office31 --public_dataset pub_fmnist --structure heterogeneity --pub_aug weak --csv_log &
@@ -56,10 +56,10 @@
 #python main.py --model fcclwehalf  --device_id 0 --dataset fl_digits --structure heterogeneity --csv_log &
 #python main.py --model fcclwehalf  --device_id 1 --dataset fl_office31 --structure heterogeneity --csv_log &
 
-python main.py --model fedrs  --device_id 5 --public_dataset pub_cifar100 --dataset fl_digits --structure homogeneity --csv_log &
-python main.py --model fedrs  --device_id 6 --public_dataset pub_cifar100 --dataset fl_office31 --structure homogeneity --csv_log &
-python main.py --model fedproc  --device_id 3 --public_dataset pub_cifar100 --dataset fl_digits --structure homogeneity --csv_log &
-python main.py --model fedproc  --device_id 7 --public_dataset pub_cifar100 --dataset fl_office31 --structure homogeneity --csv_log &
+# python main.py --model fedrs  --device_id 5 --public_dataset pub_cifar100 --dataset fl_digits --structure homogeneity --csv_log &
+# python main.py --model fedrs  --device_id 6 --public_dataset pub_cifar100 --dataset fl_office31 --structure homogeneity --csv_log &
+# python main.py --model fedproc  --device_id 3 --public_dataset pub_cifar100 --dataset fl_digits --structure homogeneity --csv_log &
+# python main.py --model fedproc  --device_id 7 --public_dataset pub_cifar100 --dataset fl_office31 --structure homogeneity --csv_log &
 
 #python main.py --model feddf  --device_id 1 --public_dataset pub_tyimagenet --dataset fl_digits --structure heterogeneity --csv_log &
 #python main.py --model feddf  --device_id 2 --public_dataset pub_fmnist --dataset fl_digits --structure heterogeneity --csv_log &
